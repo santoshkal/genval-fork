8d67f740710eed2c7843f32573907826940236f2
santoshkal <ksantosh@intelops.dev>
2023-10-05 16:42:27 +0530

    Update: REST server functionility, with genval --server flag
    Signed-off-by: santoshkal <ksantosh@intelops.dev>

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
+	"github.com/intelops/genval/pkg/api/v1"
 	generate "github.com/intelops/genval/pkg/generate/dockerfile-gen"
 	"github.com/intelops/genval/pkg/parser"
 	validate "github.com/intelops/genval/pkg/validate/dockerfile-val"
@@ -11,7 +13,18 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+var serverMode bool
+
+func init() {
+	flag.BoolVar(&serverMode, "server", false, "Run in server mode")
+	flag.Parse()
+}
 func main() {
+	if serverMode {
+		runServer()
+		return
+	}
+
 	if len(os.Args) != 3 {
 		log.Debug("Usage: go run main.go input.json output.Dockerfile")
 		return
@@ -62,5 +75,16 @@ func main() {
 		return
 	} else {
 		fmt.Printf("Dockerfile validation succeeded!\n")
+	}
+}
+
+func runServer() {
+	r := api.SetupAPI()
+	port := ":3333"
+	serverAddr := "localhost" + port
+	fmt.Printf("API server is running on port %s...\n", port)
+
+	if err := r.Run(serverAddr); err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
 	}
 }
